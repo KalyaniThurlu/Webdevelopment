db6e296b40382c1ace850a52ff5091d79ed8d8bc
kalyanithurlu <kalyanithurlu@gmail.com>
2024-04-13 11:29:00 +0530

    message

--- JS/Form.js
+++ JS/Form.js
@@ -1,20 +1,12 @@
-document
-  .getElementById("loginForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent form submission
+function validateLogin() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
 
-    // Get input values
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-
-    // Validate username and password (simple validation for demonstration)
-    if (username === "admin" && password === "password") {
-      // Successful login
-      alert("Login successful!");
-      // You can redirect to another page or perform other actions here
-    } else {
-      // Display error message
-      document.getElementById("error").textContent =
-        "Invalid username or password";
-    }
-  });
+  if (username === "") {
+    alert("enter your name");
+  } else if (password === trim()) {
+    alert("must and should 6 characters");
+  } else {
+    alert("please enter this form");
+  }
+}
--- JS/practice.js
+++ JS/practice.js
@@ -1,224 +1,60 @@
-let arr = [1, 2, 3, 4];
-let exarr = arr.push(5);
-console.log(arr);
-
-let Arr = [1, 2, 3, 4, 5];
-let exArr = Arr.pop();
-console.log(Arr);
-
-let names = ["padhma", "latha", "preethi", "mahima"];
-let exnames = names.shift();
-console.log(names);
-
-let fruits = ["apple", "banana", "guova", "grapes"];
-let exfruits = fruits.unshift("mango");
+let arry = [1, 2, 3, 4, 5];
+let exarry = arry.pop();
+console.log(arry);
+let Arry = [1, 0, 3, 4, 5, 2];
+let exArry = Arry.push(9);
+console.log(Arry);
+let frt = ["banana", "apple", "grapes"];
+let exfrt = frt.shift();
+console.log(frt);
+let fruits = ["guvo", "mango", "banana"];
+let exfruits = fruits.unshift("watermillon");
 console.log(fruits);
-
-let Filter = [1, 2, 3, 4, 5, 6, 8, 9, 7, 10];
-let ExFilter = Filter.filter(function (e) {
+let exfilter = [1, 7, 6, 5, 4, 3, 2];
+let rst = exfilter.filter(function (e) {
   return e % 2 === 0;
 });
-console.log(ExFilter);
-let map = [
-  { name: "sindhu", id: 1001 },
-  { name: "pavithra", id: 1002 },
-  { name: "sumana", id: 2002 },
-];
-let exmap = map.map(function (e) {
-  return e.id;
-});
-console.log(exmap);
-let veg = ["mirchi", "bringel", "onion"];
-let frt = ["banana", "apple", "mango"];
-let combaind = veg.concat(frt);
-console.log(combaind);
-let mobiles = ["vivo", "samsung", "mi", "red me"];
-let ExForeach = mobiles.forEach(function (v) {
-  console.log(")" + v);
-});
-let ExJoin = [1, 2, 3, 4, 5];
-let rst = ExJoin.join("*");
 console.log(rst);
-let ExSlice = [1, 2, 3];
-let results = ExSlice.slice(2);
-console.log(results);
-
-//===String Methods======
-let ExTouppercase = "hello";
-let ExStr = ExTouppercase.toUpperCase();
-console.log(ExStr);
-
-let ExTolowercase = "HEllo";
-let Exstr = ExTolowercase.toLowerCase();
-console.log(Exstr);
-
-let ExIncludes = ["suma", "preethi", "lakshmi", "meera", "mahima"];
-let exincludes = ExIncludes.includes("lakshmi");
-console.log(exincludes);
-
-let ExReplace = ["kalyani", "Geetha"];
-let ExIndexof = ["darani", "sumana", "preethi", "vimala", "padhma"];
-let exstr = ExIndexof.indexOf("preethi");
-console.log(exstr);
-
-let Exlastindexof = ["banana", "apple", "mango", "grapes"];
-let Results = Exlastindexof.lastIndexOf("grapes");
-
-console.log(Results);
-let Exsplit = "this is my first programme";
-let Answer = Exsplit.split("i");
-console.log("findingnums" + Answer);
-let ExTrimstart = "     hello";
-let Extrim = ExTrimstart.trimStart();
-console.log(Extrim);
-
-let str = "   Hello, World!   ";
-let trimmedStr = str.trimEnd();
-
-console.log(trimmedStr);
-const sentence = "Hello, World!";
-const newStr = sentence.replace("Hello", "Hi");
-
-console.log(newStr);
-///========Functionsinjs======
-function myFun(a, b) {
-  return a + b;
-}
-let RSt = myFun(2, 3);
-console.log(RSt);
-
-function Withoutparam() {
-  return "hello";
-}
-let rr = Withoutparam();
-console.log(rr);
-
-function WithoutReturn() {
-  console.log("wellcome");
-}
-WithoutReturn();
-function WithOutreturnWithparam(name, id) {
-  console.log(name + id);
-}
-WithOutreturnWithparam("kalyani", 1001);
-const myfun = (a, b) => {
-  return a + b;
-};
-console.log(myFun(29, 30));
-const withoutparam = () => {
-  return "hi";
-};
-console.log(withoutparam());
-const withoutreturn = () => {
-  console.log("this is my first programme");
-};
-withoutreturn();
-const withparam = (a, b) => {
-  console.log(a + b);
-};
-withparam(20, 20);
-//=======ForLoop==========;
-for (let i = 0; i <= 100; i++) {
-  console.log("wellcome" + i);
-}
-for (let i = 2; i <= 2; i++) {
-  for (let j = 1; j <= 10; j++) {
-    console.log(i + "*" + j + "=" + i * j);
-  }
-}
-for (let i = 1; i <= 5; i++) {
-  row = "";
-  for (let j = i; j <= 5; j++) {
-    row += "*";
-  }
-  console.log(row);
-}
-for (let i = 1; i <= 5; i++) {
-  row = "";
-  for (let j = 1; j <= i; j++) {
-    row += "*";
-  }
-  console.log(row);
-}
-let num = [1, 2, 3, 4, 5, 7, 6, 8, 9, 10];
-let evennun = [];
-for (let i = 0; i < num.length; i++) {
-  if (num[i] % 2 === 0) {
-    evennun.push(num[i]);
-  }
-}
-console.log(evennun);
-
-let Num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let oddnum = [];
-for (let i = 0; i < Num.length; i++) {
-  if (Num[i] % 2 !== 0) {
-    oddnum.push(Num[i]);
-  }
-}
-console.log(oddnum);
-
-let expositivenum = [1, -2, 3, -4, -6, 5, -7, 8];
-let Positive = [];
-for (let i = 0; i < expositivenum.length; i++) {
-  if (expositivenum[i] > 0) {
-    Positive.push(expositivenum[i]);
-  }
-}
-console.log(Positive);
-let negtive = [1, -2, -3, 4, -5, 6, -7, 8, 9, 10];
-let exnegtive = [];
-for (let i = 0; i < negtive.length; i++) {
-  if (negtive[i] < 0) {
-    exnegtive.push(negtive[i]);
-  }
-}
-console.log(exnegtive);
-let exbignum = [10, 20, 30, 40, 50, 60, 100, 200];
-let bignum = 20;
-for (let i = 0; i < exbignum.length; i++) {
-  if (exbignum[i] > bignum) {
-    bignum = exbignum[i];
-  }
-}
-console.log(bignum);
-let exminnum = [1, 0, 3, 4, 6, 9, 8, 5, -1];
-let minnum = 5;
-for (let i = 0; i < exminnum.length; i++) {
-  if (exminnum[i] < minnum) {
-    minnum = exminnum[i];
-  }
-}
-console.log(minnum);
-const emp = [
-  { name: "kalyani", id: 1001 },
-  { name: "latha", id: 1002 },
-  { name: "Geetha", id: 1003 },
+let exmap = [
+  { name: "latha", id: 100 },
+  { name: "geetha", id: 1002 },
+  { name: "suma", id: 1003 },
 ];
-console.table(emp.filter((e) => e.name.startsWith("l")));
-const employee = [
-  { name: "padhma", Dob: 1991 },
-  { name: "mahima", Dob: 2002 },
-  { name: "preethi", Dob: 2004 },
-];
-const currentyear = new Date().getFullYear();
-for (let employees of employee) {
-  employees.age = currentyear - employees.Dob;
-}
-console.table(employee);
-//===logical oparetors======;
-let a = 10,
-  b = 20,
-  c = 30;
-const My = () => {
-  if (a === b) {
-    console.log("a is eqults to b");
-  } else if (c > b) {
-    console.log("c is greater than b");
-  } else if (a > b || c == a) {
-    console.log("a is lessthan b are c eqults to a");
-  } else {
-    console.log("All are wrong");
-  }
-};
-My();
+let names = exmap.map(function (v) {
+  return v.id;
+});
+console.log(names);
+let exforeach = ["apple", "orange", "guava", "banana"];
+let Frt = exforeach.forEach(function (c) {
+  console.log(")" + c);
+});
+let veg = ["bringel", "ledifinger", "onion", "mirchi"];
+let exveg = veg.join("*");
+console.log(exveg);
+let std = "this is my first programme";
+let exstd = std.slice(5 - 12);
+console.log(exstd);
+let student = ["latha", "preethi", "mahima", "suma", "ramana"];
+let orr = student.splice(1 - 3);
+console.log(orr);
+let Veg = ["mirchi", "onion", "bringel", "ledifinger"];
+let exFrt = ["orange", "mango", "banana", "grapes"];
+let conbained = Veg.concat(exFrt);
+console.log(conbained);
+//=====String Methods=======
+let word = "hello";
+let exword = word.toUpperCase();
+console.log(exword);
+
+let Sentence = "THIS IS MY FIRST PROGRAMME";
+let exsentences = Sentence.toLowerCase();
+console.log(exsentences);
+
+let subject = ["telugu", "hindhi", "english"];
+let exsubject = subject.includes("hindhi");
+console.log(exsubject);
+
+let world = ["kalyani      hello"];
+let exworld = word.replace("hello", "hi");
+console.log(exworld);
+let sen = ["this is my first  class"];
