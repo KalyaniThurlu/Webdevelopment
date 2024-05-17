984ebc8d87236f113144541d56d747c15706b50f
kalyanithurlu <kalyanithurlu@gmail.com>
2024-05-17 21:49:37 +0530

    updated

---
+++ CSS/HTML/cartoons/script.js
@@ -0,0 +1,55 @@
+const data = [
+  {
+    title: "Spongebob Squarepants",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeUTOfI1NenpdpNvh0T_o494ZQK79dCmiFfBx8a5iu5UPJ0f_O5l_grsEMMdFW-P_fk3k&usqp=CAU",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quamLorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quam",
+  },
+  {
+    title: "Chota Bheem",
+    image:
+      "https://mir-s3-cdn-cf.behance.net/projects/404/8937ea156344861.Y3JvcCwxMzg3LDEwODUsMjY3LDA.jpg",
+
+    description:
+      "ipisicing elit. Reprehenderit, quamLorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quam",
+  },
+  {
+    title: "Mickey Mouse",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_aIH92G1Ww6grDmZOux7w6b36mw2u2hcSks5WYtiLGg&s",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quamLorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quam",
+  },
+  {
+    title: "Mooshak Gungun",
+    image:
+      "https://i.pinimg.com/564x/46/94/73/469473fe7889a93d10a2a47eec5d193a.jpg",
+
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+];
+
+const maindiv = document.getElementById("library");
+
+function displayData(data) {
+  data.forEach((element) => {
+    const sameDiv = document.createElement("div");
+    sameDiv.classList.add("cartoon");
+
+    const heading = document.createElement("h2");
+    heading.textContent = element.title;
+
+    const image = document.createElement("img");
+    image.src = element.image;
+    image.alt = element.title;
+
+    const description = document.createElement("p");
+    description.textContent = element.description;
+
+    sameDiv.append(heading, image, description);
+    maindiv.appendChild(sameDiv);
+  });
+}
+
+displayData(data);
