10fa31b6a9ec62455cb2125e51096cae5dff01da
kalyanithurlu <kalyanithurlu@gmail.com>
2024-05-23 18:36:37 +0530

    updated

--- JS/Api2.js
+++ JS/Api2.js
@@ -1,7 +1,8 @@
 const mainDiv = document.getElementById("image");
-
 function fetchData() {
-  fetch("https://api.thecatapi.com/v1/images/search").then(function (res) {
+  fetch("  https://api.thecatapi.com/v1/images/search?limit=10").then(function (
+    res
+  ) {
     console.log(res);
     return res.json().then(function (data) {
       console.log(data);
@@ -9,13 +10,12 @@ function fetchData() {
     });
   });
 }
-
 function displayData(data) {
   mainDiv.textContent = "";
   const img = document.createElement("img");
   img.src = data.url;
-  img.alt = "cat-image";
-  img.style.width = "200px";
+  img.alt = "cat-img";
+  img.style.width = "100px";
   img.style.height = "200px";
   mainDiv.appendChild(img);
 }
